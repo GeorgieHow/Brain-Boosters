350444646881630575d8b6b436ebc9ef7c0f6059
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-03 18:36:58 +0000

    Added firebase authentication on as well as a test user. Tested to see if it can check firebase for that user.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -51,6 +53,8 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -59,6 +63,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/brainboosters/MainActivity.kt
+++ app/src/main/java/com/example/brainboosters/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.brainboosters
 
+import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,36 +16,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.brainboosters.ui.theme.BrainBoostersTheme
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : ComponentActivity() {
+
+    private var mAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            BrainBoostersTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Test")
+        setContentView(R.layout.activity_main)
+
+        val emailEntered = findViewById<EditText>(R.id.emailText)
+        val passwordEntered = findViewById<EditText>(R.id.passwordText)
+        val enterButton = findViewById<Button>(R.id.enterButton)
+
+        enterButton.setOnClickListener{view ->
+            mAuth.signInWithEmailAndPassword(
+                emailEntered.text.toString(),
+                passwordEntered.text.toString()
+            )
+                .addOnCompleteListener(
+                    this
+                ){task ->
+                    if (task.isSuccessful){
+                        //remove done and make them go to home page LOL.
+                        emailEntered.setText("DONE")
+                    }
+
                 }
-            }
+
         }
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BrainBoostersTheme {
-        Greeting("Test")
-    }
-}
+
