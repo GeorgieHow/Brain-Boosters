7be1ea7889c9647579941e489ec2ec995ce25e4b
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-04 21:20:21 +0000

    Notification now appears if account does or does not exist. App no longer crashes with NULL username or password.

--- app/src/main/java/com/example/brainboosters/MainActivity.kt
+++ app/src/main/java/com/example/brainboosters/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,30 +24,50 @@ import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
 
+    //Establishes firebase authentication
     private var mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Log-in page
         setContentView(R.layout.activity_main)
 
-        val emailEntered = findViewById<EditText>(R.id.emailText)
-        val passwordEntered = findViewById<EditText>(R.id.passwordText)
         val enterButton = findViewById<Button>(R.id.enterButton)
 
-        enterButton.setOnClickListener{view ->
-            mAuth.signInWithEmailAndPassword(
-                emailEntered.text.toString(),
-                passwordEntered.text.toString()
-            )
-                .addOnCompleteListener(
-                    this
-                ){task ->
-                    if (task.isSuccessful){
-                        //remove done and make them go to home page LOL.
-                        emailEntered.setText("DONE")
-                    }
+        //Checks if user has an account and entered details are correct
+        enterButton.setOnClickListener{
+                view ->
 
-                }
+            //Gets the ids for the fields and buttons
+            val emailEntered = findViewById<EditText>(R.id.emailText)
+            val passwordEntered = findViewById<EditText>(R.id.passwordText)
+
+            val isEmailEmpty = emailEntered.text.toString()
+            val isPasswordEmpty = passwordEntered.text.toString()
+
+            if(isEmailEmpty.isNotEmpty() && isPasswordEmpty.isNotEmpty()) {
+
+                mAuth.signInWithEmailAndPassword(
+                    emailEntered.text.toString(),
+                    passwordEntered.text.toString()
+                )
+                    .addOnCompleteListener(
+                        this
+                    ) { task ->
+                        if (task.isSuccessful) {
+                            //remove done and make them go to home page LOL.
+                            Toast.makeText(this, "Account does exist", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this, "Account does not exist", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this, "Don't leave fields empty", Toast.LENGTH_SHORT)
+                    .show()
+            }
 
         }
 
