d62d2a952ad289d406709345a4267c939274a0d6
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-14 22:20:30 +0000

    Added log out button to home page.

--- app/src/main/java/com/example/brainboosters/HomePageActivity.kt
+++ app/src/main/java/com/example/brainboosters/HomePageActivity.kt
@@ -1,11 +1,15 @@
 package com.example.brainboosters
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class HomePageActivity : ComponentActivity() {
+
+    private var mAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -13,12 +17,19 @@ class HomePageActivity : ComponentActivity() {
         setContentView(R.layout.home_page)
 
         val userWelcome = findViewById<TextView>(R.id.userEmailTextView)
-        val user = FirebaseAuth.getInstance().currentUser
+        val user = mAuth.currentUser
 
         if (user != null){
-            userWelcome.setText(user.email)
+            userWelcome.setText("Welcome " + user.email)
         } else {
             userWelcome.setText("User does not exist.")
+        }
+
+
+        val logOutButton = findViewById<Button>(R.id.logOutButton)
+        logOutButton.setOnClickListener{
+            mAuth.signOut()
+            finish()
         }
 
     }
