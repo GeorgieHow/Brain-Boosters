55f4a1adb2e9cc3e07b0326797349e0300f80eca
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-14 22:11:23 +0000

    Users can create an account.

--- app/src/main/java/com/example/brainboosters/SignUpPageActivity.kt
+++ app/src/main/java/com/example/brainboosters/SignUpPageActivity.kt
@@ -3,11 +3,17 @@ package com.example.brainboosters
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpPageActivity : ComponentActivity() {
+
+    private var mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +26,51 @@ class SignUpPageActivity : ComponentActivity() {
         animDrawable.setExitFadeDuration(5000)
         animDrawable.start()
 
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
+        signUpButton.setOnClickListener{
+                view ->
+
+            val emailEntered = findViewById<EditText>(R.id.emailText)
+            val passwordEntered = findViewById<EditText>(R.id.passwordText)
+
+            val passwordRetyped = findViewById<EditText>(R.id.passwordRetypeText)
+
+            val emailString = emailEntered.text.toString()
+            val passwordString = passwordEntered.text.toString()
+
+            val passwordRetypedString = passwordRetyped.text.toString()
+
+            if(emailString.isNotEmpty() && passwordString.isNotEmpty()){
+                if(passwordString == passwordRetypedString){
+
+                    mAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "Account Created",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                            else{
+                                Toast.makeText(this, "Account Unsuccessful ",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+                }
+            }
+            else{
+                Toast.makeText(this, "Don't leave fields empty", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
         val backButton = findViewById<Button>(R.id.backButton)
         backButton.setOnClickListener{
             finish()
         }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
