e08877d467ba78b4cb361165df3ec039fed6ac6a
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-14 15:16:56 +0000

    Can get the user that is currently logged in.

--- app/src/main/java/com/example/brainboosters/HomePageActivity.kt
+++ app/src/main/java/com/example/brainboosters/HomePageActivity.kt
@@ -1,11 +1,25 @@
 package com.example.brainboosters
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class HomePageActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Home page
         setContentView(R.layout.home_page)
+
+        val userWelcome = findViewById<TextView>(R.id.userEmailTextView)
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null){
+            userWelcome.setText(user.email)
+        } else {
+            userWelcome.setText("User does not exist.")
+        }
+
     }
 }
