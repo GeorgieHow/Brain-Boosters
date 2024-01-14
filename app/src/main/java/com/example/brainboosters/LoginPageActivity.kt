cc25007fd1dfb665ad3a2c5811c26ecb2d5e8b50
Georgie Howarth <106632575+GeorgieHow@users.noreply.github.com>
2024-01-14 18:05:33 +0000

    Login page now has similar look to welcome page.

--- app/src/main/java/com/example/brainboosters/LoginPageActivity.kt
+++ app/src/main/java/com/example/brainboosters/LoginPageActivity.kt
@@ -1,11 +1,13 @@
 package com.example.brainboosters
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -16,9 +18,14 @@ class LoginPageActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
 
-        //Log-in page
-        setContentView(R.layout.activity_main)
+        val rootLayout = findViewById<ConstraintLayout>(R.id.login_page_layout)
+        val animDrawable = rootLayout.background as AnimationDrawable
+
+        animDrawable.setEnterFadeDuration(10)
+        animDrawable.setExitFadeDuration(5000)
+        animDrawable.start()
 
         val enterButton = findViewById<Button>(R.id.enterButton)
 
